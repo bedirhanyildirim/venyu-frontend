e41d3a33f27b4b3700339fe8e43b58c1a3c5f32b
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-04-05 13:57:58 +0200

    feat: react-redux installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import {useSelector} from "react-redux";
+
 export default function Home() {
+  
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+  
   return (
     <div className="max-w-screen-xl w-full">
       <h1 className="text-3xl font-bold">Hoşgeldiniz</h1>
       <p className="text-xl mt-2">Ev Kiralamanın En Güvenilir Yolu</p>
+      <br/>
+      <p>Is user logged in? {isLoggedIn ? 'Yes' : 'No'}</p>
     </div>
   )
 }
