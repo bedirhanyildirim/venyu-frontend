2bbe8247777b22830cd1a7d694bf7417372b3e5b
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-05-06 17:16:33 +0200

    feat: logout button added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,21 @@
 import { NavLink } from "react-router-dom"
+import { auth } from "../firebase/firebase.config"
+import { signOut } from "firebase/auth"
+import { useDispatch } from "react-redux"
+import { setUser } from "../stores/auth"
 
 export default function Header() {
+  
+  const dispatch = useDispatch()
+  
+  const logout = () => {
+    signOut(auth).then(() => {
+      dispatch(setUser({}))
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+  
   return (
     <div className="w-full flex justify-center items-center bg-gray-100 bg-white">
       <div className="content flex justify-between items-center gap-4 py-2">
@@ -16,9 +31,13 @@ export default function Header() {
         <NavLink to="/signup">
           Üye Ol
         </NavLink>
+        <NavLink to="/profile">
+          Profilim
+        </NavLink>
         <NavLink to="/about" className="mr-4 md:mr-0">
           Hakkımızda
         </NavLink>
+        <div className="hover:cursor-pointer" onClick={logout}>Çıkış Yap</div>
       </div>
     </div>
   )
