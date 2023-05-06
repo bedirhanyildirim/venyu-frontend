7a25b9a10e1055c60a0a06522fd9b7e617793385
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-05-06 17:14:32 +0200

    feat: privateRoute developed for only signed in users

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,6 +2,7 @@ import Home from "../pages/Home"
 import About from "../pages/About"
 import Login from "../pages/Login"
 import Signup from "../pages/Signup.jsx"
+import PrivateRoute from "../components/privateRoute"
 
 import GuestUser from "../layouts/GuestUser"
 
@@ -30,4 +31,14 @@ const routes = [
   }
 ]
 
-export default routes
+const authCheck = routes => routes.map(route => {
+  if (route?.auth) {
+    route.element = <PrivateRoute>{ route.element }</PrivateRoute>
+  }
+  if (route?.children) {
+    route.children = authCheck(route.children)
+  }
+  return route
+})
+
+export default authCheck(routes)
