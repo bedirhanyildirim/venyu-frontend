c45427c1b54e34c243d11746a9991e96acc9e7aa
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-05-23 14:04:31 +0200

    feat: add firestore collections to the config

--- src/firebase/firebase.config.jsx
+++ src/firebase/firebase.config.jsx
@@ -1,5 +1,6 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore, collection } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "xxx",
@@ -12,5 +13,9 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const activitiesCollection = collection(db, 'activities')
+export const companiesCollection = collection(db, 'companies')
+export const sourcesCollection = collection(db, 'sources')
 export const auth = getAuth(app)
 export default app
