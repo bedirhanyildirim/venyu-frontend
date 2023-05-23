31bb8cd23554460a8e27bd29cf83684d8a18836f
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-05-23 14:07:07 +0200

    feat: create sources redux states

--- src/stores/index.jsx
+++ src/stores/index.jsx
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit"
 
 import auth from './auth.jsx'
 import loader from './loader.jsx'
+import sources from './sources.jsx'
 
 const store = configureStore({
   reducer: {
     auth,
-    loader
+    loader,
+    sources
   },
 })
 
---
+++ src/stores/sources.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  sharedSources: [],
+  lastUpdateSharedSources: undefined,
+  mySources: [],
+  lastUpdateMySources: undefined
+}
+
+const sources = createSlice({
+  name: 'sources',
+  initialState,
+  reducers: {
+    setSharedSources: (state, action) => {
+      state.sharedSources = action.payload
+      const now = new Date()
+      state.lastUpdateSharedSources = now.toISOString()
+    },
+    setMySources: (state, action) => {
+      state.mySources = action.payload
+      const now = new Date()
+      state.lastUpdateMySources = now.toISOString()
+    }
+  },
+})
+
+export const { setSharedSources, setMySources } = sources.actions
+export default sources.reducer
